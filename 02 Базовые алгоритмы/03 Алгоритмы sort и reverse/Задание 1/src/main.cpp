55b5603233c4b55d8e701a0623adc54728eb9205
bel-sndct <79698612+bel-sndct@users.noreply.github.com>
2025-08-14 19:11:40 +0300

    add th algo

---
+++ 02 Базовые алгоритмы/03 Алгоритмы sort и reverse/Задание 1/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; ++i) {
+        string name;
+        int age;
+        cin >> name >> age;
+        // сохраните в вектор пар
+    }
+
+    // выведите только имена в порядке убывания возраста
+    // Jack
+    // John
+    // ...
+}
